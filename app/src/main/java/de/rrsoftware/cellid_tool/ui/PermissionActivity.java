283cd15e053d146dde620fe7dc36fd14bba37fbf
RocketRider <e-mail@rocketrider.eu>
2017-01-22 18:14:53 +0100

    Ask for permissions

---
+++ app/src/main/java/de/rrsoftware/cellid_tool/ui/PermissionActivity.java
@@ -0,0 +1,54 @@
+package de.rrsoftware.cellid_tool.ui;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+import de.rrsoftware.cellid_tool.R;
+import de.rrsoftware.cellid_tool.camera.CameraActivity;
+
+public class PermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+        askForPermissions();
+    }
+
+
+    private void askForPermissions() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.RECEIVE_BOOT_COMPLETED
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.areAllPermissionsGranted()) {
+                    Intent intent = new Intent(PermissionActivity.this, ListLocationsActivity.class);
+                    startActivity(intent);
+                } else {
+                    askForPermissions();
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).onSameThread().check();
+    }
+}
